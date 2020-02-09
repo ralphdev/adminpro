040621b48eb0607f73d060003c981d897cd6ab7f
Rafael <rafa.developers@gmail.com>
2020-02-09 13:45:22 -0500

    Update Core 9

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef }
 })
 export class IncrementadorComponent implements OnInit {
 
-	@ViewChild('txtProgress') txtProgress: ElementRef;
+	@ViewChild('txtProgress', { static: true }) txtProgress: ElementRef;
 
   @Input('nombre') leyenda: string = 'Leyenda';
   @Input() progreso: number = 50;
